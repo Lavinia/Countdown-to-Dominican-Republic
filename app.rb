87dc0cab0922381d543332d4f13d201d0cf3cad2
Kim Persson <forgetmenotox@notingham.se>
2012-09-30 16:36:07 +0200

    Add haml view and basic temperature readings.

--- app.rb
+++ app.rb
@@ -9,11 +9,13 @@ SECONDS_PER_DAY = 86400
 
 get '/' do
   time_remaining = DEPARTURE_DATE - Time.now
-  days_remaining = (time_remaining / SECONDS_PER_DAY).to_i
-  # days_remaining.to_s
+  @days_to_departure = (time_remaining / SECONDS_PER_DAY).to_i
 
   client = Weatherman::Client.new
   weather = client.lookup_by_woeid ARUBA_WOEID
-  # weather.description
-  days_remaining.to_s + " days to departure<br>" + weather.description
+  @image = weather.description_image.values.first
+  @forecast_low = weather.forecasts.first['low']
+  @forecast_high = weather.forecasts.first['high']
+  @temp = weather.condition['temp']
+  haml :countdown
 end
