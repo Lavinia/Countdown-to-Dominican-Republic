822aa97d7dcee4651e5ee91b26c8cb72967dbab7
Kim Persson <forgetmenotox@notingham.se>
2012-10-14 13:37:06 +0200

    Add weather endpoint. DRY
    Signed-off-by: Kim Persson <forgetmenotox@notingham.se>

--- app.rb
+++ app.rb
@@ -11,9 +11,28 @@ DEPARTURE_DATE = Time.new(2012, 12, 18, 0, 0, 0, "+01:00")
 SECONDS_PER_DAY = 86400
 
 get '/' do
+  calculate_days_to_departure
+  fetch_current_weather_info
+  haml :countdown
+end
+
+get '/countdown.js' do
+  coffee :countdown
+end
+
+get '/weather' do
+  fetch_current_weather_info
+  haml :weather
+end
+
+private
+
+def calculate_days_to_departure
   time_remaining = DEPARTURE_DATE - Time.now
   @days_to_departure = (time_remaining / SECONDS_PER_DAY).ceil
+end
 
+def fetch_current_weather_info
   client = Weatherman::Client.new
   weather = client.lookup_by_woeid ARUBA_WOEID
   @image = weather.description_image.values.first
@@ -21,9 +40,4 @@ get '/' do
   @forecast_high = weather.forecasts.first['high']
   @current_temperature = weather.condition['temp']
   @current_condition = weather.condition['text']
-  haml :countdown
-end
-
-get '/countdown.js' do
-  coffee :countdown
 end
